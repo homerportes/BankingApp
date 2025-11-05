2fb8954a810b04a6f212d556f77b1fc0f16eb6c3
Johaly <johalypolanco13@gmail.com>
2025-11-04 19:12:41 -0500

    Estructurando el WebApp

---
+++ BankingApp/Areas/Client/Controllers/ClientController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankingApp.Areas.Client.Controllers
+{
+    [Authorize(Roles = "Client")]
+
+    public class ClientController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Home() { return View(); } // Product list
+        public IActionResult Beneficiaries() { return View(); }
+        public IActionResult Transactions() { return View(); }
+        public IActionResult CashAdvances() { return View(); }
+        public IActionResult AccountTransfers() { return View(); }
+    }
+}
---
+++ BankingApp/Areas/Teller/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BankingApp.Areas.Teller.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
